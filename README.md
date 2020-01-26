63868cc05458108906ad0170419aefbbc77e627e
Phil Wamba <philwamba@gmail.com>
2020-01-26 17:53:14 +0300

    Added jquery & implemented sticky nav

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 <img src="img/screenshot2.jpg" alt="Screenshot">
 
 ## :book: Todos
+ - Complete blog, gallery & about page
  - Implement browser fixes
  - Fix responsive view
---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+/*================
+ Template Name: Explorer Boootrap Template
+ Description: A responsive tourism bootstrap template .
+ Version: 1.0.0
+ Author: Phil Wamba
+ =======================*/
+
+ jQuery(function ($) {
+
+    'use strict';
+
+    $(window).scroll(function() {
+        if ($(".navbar").offset().top > 100) {
+            $(".navbar.fixed-top").addClass("sticky-nav");
+        } else {
+            $(".navbar.fixed-top").removeClass("sticky-nav");
+        }
+    });
+
+})(); // JQuery end
